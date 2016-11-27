6be0dc14324fa77b94a2c20db8e0d89d1316c73f
Newton Jain <newton.jain@gmail.com>
2016-11-27 14:23:19 -0800

    Removing tests and extra stuff not needed for demo

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+var glimpse = require('@glimpse/glimpse-node');
+glimpse.init();
+
+/**
+ * Module dependencies.
+ */
+var app = require('./config/lib/app');
+var server = app.start();
